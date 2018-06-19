2b95b32938cd217f61ee3e008e8b0b7792b5b101
yrocha <yanrochalves@gmail.com>
2018-06-19 19:21:22 -0400

    Initial commit.

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <hello-component name="World" />
+  </div>
+</template>
+<script>
+
+import HelloComponent from './components/hello/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
